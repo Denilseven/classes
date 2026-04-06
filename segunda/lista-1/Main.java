e30c181063999a261e47c97205f1575ecadd7dc7
Denilseven <104108067+Denilseven@users.noreply.github.com>
2026-04-06 10:15:15 -0400

    lista POO: exercicio 3

--- segunda/lista-1/Main.java
+++ segunda/lista-1/Main.java
@@ -4,6 +4,7 @@
  * Aluno: Denilson
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Carro {
@@ -31,10 +32,14 @@ public class Carro {
 
 public class Main {
     public static void main(String[] args) {
-        Carro carro1 = new Carro("Wolfsagen", "AT09", 2010);
-        Carro carro2 = new Carro("Tóiot", "Horizon", 2017);
+        ArrayList<Carro> carros = new ArrayList<Carro>();
 
-        carro1.exibir();
-        carro2.exibir();
+        carros.add(new Carro("Wolfsagen", "AT09", 2010));
+        carros.add(new Carro("Tóiot", "Horizon", 2017));
+        carros.add(new Carro("Ronday", "Koloral", 1830));
+
+        carros.forEach(carro -> carro.exibir());
+        // Esse `forEach` é bem parecido com isso:
+        // https://github.com/Denilseven/breakout-cppcon2014/blob/9e3c0501ea61c9fc66f6c6a5618a0dd92c3874cc/main.cxx#L363
     }
 }
