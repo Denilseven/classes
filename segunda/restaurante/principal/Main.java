4e442c33f5a425ecf59e0abb9d29d4fbd5afb496
Denilseven <104108067+Denilseven@users.noreply.github.com>
2026-04-07 17:29:20 -0400

    restaurante POO: adicionar cardápio, carrinho e modificação do carrinho

--- segunda/restaurante/principal/Main.java
+++ segunda/restaurante/principal/Main.java
@@ -1,12 +1,19 @@
 package principal;
 import customizado.CustomizadoIO;
+import java.util.ArrayList;
 
 public class Main {
     public static Banco banco = new Banco();
     public static Usuario usuarioLogado = null;
+    public static ArrayList<String> cardapio = new ArrayList<String>();
 
     public static void main(String[] args) {
         banco.cadastrarUsuario("admin", "admin"); // "Cheat code"
+
+        cardapio.add("Cachorro-Quente");
+        cardapio.add("Sushi");
+        cardapio.add("Pizza");
+        cardapio.add("Baguncinha");
 
         CustomizadoIO.imprimirTitulo("LOGIN");
 
@@ -39,6 +46,10 @@ public class Main {
         while (repetir) {
             CustomizadoIO.imprimirTitulo("MENU");
             System.out.println("0) Sair");
+            System.out.println("1) Exibir cardápio");
+            System.out.println("2) Exibir meu carrinho");
+            System.out.println("3) Adicionar item ao carrinho");
+            System.out.println("4) Remover item do carrinho");
 
             int input = CustomizadoIO.lerNumero();
             switch (input) {
@@ -46,11 +57,52 @@ public class Main {
                 usuarioLogado = null;
                 repetir = false;
                 break;
+            case 1:
+                CustomizadoIO.imprimirTitulo("CARDÁPIO");
+                imprimirCardapio();
+                CustomizadoIO.aguardarEnter();
+                break;
+            case 2:
+                CustomizadoIO.imprimirTitulo("MEU CARRINHO");
+                usuarioLogado.getCarrinho().forEach(item -> System.out.println(item));
+                CustomizadoIO.aguardarEnter();
+                break;
+            case 3:
+                CustomizadoIO.imprimirTitulo("ADICIONAR AO CARRINHO");
+                imprimirCardapio();
+                int adicionarItemIndex = CustomizadoIO.lerNumero();
+                if (adicionarItemIndex >= 0 && adicionarItemIndex < cardapio.size()) {
+                    usuarioLogado.adicionarAoCarrinho(cardapio.get(adicionarItemIndex));
+                    System.out.println(cardapio.get(adicionarItemIndex) + " adicionado(a) ao carrinho!");
+                }
+                else {
+                    System.out.println("Escolha inválida!");
+                }
+                CustomizadoIO.aguardarEnter();
+                break;
+            case 4:
+                CustomizadoIO.imprimirTitulo("REMOVER DO CARRINHO");
+                for (int i = 0; i < usuarioLogado.getCarrinho().size(); i++)
+                    System.out.println(i + ") " + usuarioLogado.getCarrinho().get(i));
+                int removerItemIndex = CustomizadoIO.lerNumero();
+                String itemRemovido = usuarioLogado.removerDoCarrinho(removerItemIndex);
+                if (itemRemovido != null)
+                    System.out.println(itemRemovido + " removido com sucesso!");
+                else
+                    System.out.println("Escolha inválida!");
+                CustomizadoIO.aguardarEnter();
+                break;
             default:
                 System.out.println("Opção inválida!");
                 CustomizadoIO.aguardarEnter();
                 break;
             }
+        }
+    }
+
+    public static void imprimirCardapio() {
+        for (int i = 0; i < cardapio.size(); i++) {
+            System.out.println(i + " --- " + cardapio.get(i));
         }
     }
 }
--- segunda/restaurante/principal/Usuario.java
+++ segunda/restaurante/principal/Usuario.java
@@ -4,10 +4,12 @@
 // isso tudo então fazer o quê né ,_,
 
 package principal;
+import java.util.ArrayList;
 
 public class Usuario {
     private String nome;
     private String senha; // WARNING: "Segurança 10000%"
+    private ArrayList<String> carrinho;
 
     public String getNome() { return this.nome; }
     public void setNome(String nome) { this.nome = nome; }
@@ -15,9 +17,35 @@ public class Usuario {
     public Usuario(String nome, String senha) {
         this.nome = nome;
         this.senha = senha;
+        this.carrinho = new ArrayList<String>();
     }
 
     public boolean validar(String senha) {
         return this.senha.equals(senha);
+    }
+
+    public void adicionarAoCarrinho(String item) {
+        this.carrinho.add(item);
+    }
+    public String removerDoCarrinho(String item) { // Bro pq vc escreveu essa função lmao nem precisou também
+        for (int i = 0; i < this.carrinho.size(); i++) {
+            if (this.carrinho.get(i).equals(item)) {
+                String result = this.carrinho.get(i);
+                this.carrinho.remove(i);
+                return result;
+            }
+        }
+        return null;
+    }
+    public String removerDoCarrinho(int index) {
+        if (index >= 0 && index < this.carrinho.size()) {
+            String result = this.carrinho.get(index);
+            this.carrinho.remove(index);
+            return result;
+        }
+        return null;
+    }
+    public ArrayList<String> getCarrinho() {
+        return this.carrinho;
     }
 }
